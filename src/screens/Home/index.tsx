758305ef9562a0d20d62a911086c604019744941
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-07-29 20:05:08 -0300

    Create global theme

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,13 @@
+
+ import React, { ReactElement } from 'react';
+ 
+ import { Container, Title } from './styles';
+ 
+ export default function Home(): ReactElement {
+   return (
+     <Container>
+       <Title>HOME</Title>
+     </Container>
+   );
+ }
+ 
