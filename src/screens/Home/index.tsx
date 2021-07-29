acd6a9712f84eefb230cb6f58587a91f3f5a4b61
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-07-29 20:37:17 -0300

    Create header of screen Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,33 @@
 
- import React, { ReactElement } from 'react';
+import React, { ReactElement } from 'react';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import Logo from '../../assets/logo.svg';
+
+import { 
+   Container, 
+   Header,
+   HeaderContent,
+   TotalCards
+} from './styles';
  
- import { Container, Title } from './styles';
- 
- export default function Home(): ReactElement {
-   return (
-     <Container>
-       <Title>HOME</Title>
-     </Container>
-   );
- }
+export default function Home(): ReactElement {
+  return (
+    <Container>
+      <StatusBar 
+        barStyle="light-content" 
+        backgroundColor="transparent" 
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+
+          <TotalCards>Total de 12 carros</TotalCards>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
+}
  
