a3e8a26ae2ebc90c78aadebd76c0bf39462b49fa
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-07-29 21:33:25 -0300

    Create component car

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,6 +3,8 @@ import React, { ReactElement } from 'react';
 import { StatusBar } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
+import Car from '../../components/Car';
+
 import Logo from '../../assets/logo.svg';
 
 import { 
@@ -13,6 +15,26 @@ import {
 } from './styles';
  
 export default function Home(): ReactElement {
+  const carData = {
+    brand: 'Audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'Ao dia',
+      price: 120,
+    },
+    thumbnail: 'https://img2.gratispng.com/20180618/hwg/kisspng-2015-audi-a6-2016-audi-a6-2018-audi-a6-audi-s6-2016-audi-a6-30t-premium-plus-5b27746431f124.5210989915293123562046.jpg'
+  }
+
+  const carData2 = {
+    brand: 'Porche',
+    name: 'Panamera',
+    rent: {
+      period: 'Ao dia',
+      price: 340,
+    },
+    thumbnail: 'https://img2.gratispng.com/20180618/bzc/kisspng-mid-size-car-porsche-panamera-4-e-hybrid-sport-tur-2018-porsche-panamera-5b2814804a5f90.7485803615293533443046.jpg'
+  }
+
   return (
     <Container>
       <StatusBar 
@@ -27,6 +49,8 @@ export default function Home(): ReactElement {
           <TotalCards>Total de 12 carros</TotalCards>
         </HeaderContent>
       </Header>
+      <Car data={carData} />
+      <Car data={carData2} />
     </Container>
   );
 }
