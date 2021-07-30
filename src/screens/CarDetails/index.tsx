e4503eb068e81d6d16a09d931c93597151b26892
Davi Hoffmann <davi.phoffmann@gmail.com>
2021-07-30 20:09:39 -0300

    Create component Image Slider

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import BackButton from '../../components/BackButton';
+import ImageSlider from '../../components/ImageSlider';
 
 import {
   Container,
-  Header
+  Header,
+  CarImages
 } from './styles';
 
 export default function CarDetails() {
@@ -13,6 +15,12 @@ export default function CarDetails() {
       <Header>
         <BackButton onPress={() => {}} />
       </Header>
+
+      <CarImages>
+        <ImageSlider 
+          imageUrl={['https://img2.gratispng.com/20180618/hwg/kisspng-2015-audi-a6-2016-audi-a6-2018-audi-a6-audi-s6-2016-audi-a6-30t-premium-plus-5b27746431f124.5210989915293123562046.jpg']} 
+        />
+      </CarImages>
     </Container>
   )
 }
